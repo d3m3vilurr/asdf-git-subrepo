5297cb1673a888907af2eaafe7cd19e44f4e52bc
Lee, SungUk <sulee@ea.com>
2023-11-16 13:18:11 +0900

    lib.utils: Filter old invalid tag such as 216.0 and 216.1

--- lib/utils.bash
+++ lib/utils.bash
@@ -26,7 +26,8 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		sed 's/^v//' | # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		grep -e '[0-9]\+\.[0-9]\+\.[0-9]\+'
 }
 
 list_all_versions() {
